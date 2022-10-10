ab8474c186cb75a4991816eb80b5264edf34b955
Daan Vos de Wael <daan@vosdewael.com>
2022-10-10 17:56:18 +0200

    feat: change config to use tabs + reindent source files

--- index.js
+++ index.js
@@ -1,94 +1,90 @@
 /* eslint-disable unicorn/prefer-module */
 
 module.exports = {
-    extends: ['stylelint-config-standard'],
-    overrides: [
-        {
-            customSyntax: 'postcss-html',
-            files: ['*.vue', '**/*.vue'],
-        },
-    ],
-    plugins: ['stylelint-order'],
-    rules: {
-        'color-named': 'never',
-        'declaration-block-no-redundant-longhand-properties': [
-            true,
-            {
-                ignoreShorthands: ['/grid/'],
-            },
-        ],
-        'declaration-block-semicolon-newline-after': 'always',
-        'declaration-no-important': true,
-        'declaration-property-value-disallowed-list': {
-            '/^border/': ['none'],
-        },
-        'font-weight-notation': 'numeric',
-        'indentation': 4,
-        'max-line-length': null,
-        'max-nesting-depth': [
-            4,
-            {
-                ignore: [
-                    'pseudo-classes',
-                ],
-                ignoreAtRules: [
-                    'media',
-                    'supports',
-                ],
-            },
-        ],
-        'no-descending-specificity': null,
-        'number-max-precision': 6,
-        'order/order': [
-            [
-                'custom-properties',
-                'dollar-variables',
-                {
-                    name: 'extend',
-                    type: 'at-rule',
-                },
-                {
-                    hasBlock: false,
-                    name: 'include',
-                    type: 'at-rule',
-                },
-                'declarations',
-                {
-                    hasBlock: true,
-                    name: 'include',
-                    type: 'at-rule',
-                },
-                'rules',
-            ],
-        ],
-        'order/properties-alphabetical-order': true,
-        'selector-class-pattern': [
-            '^[a-z0-9\\-]+$',
-            {
-                message: 'Selector should be written in lowercase with hyphens',
-            },
-        ],
-        'selector-max-compound-selectors': 3,
-        'selector-max-id': 0,
-        'selector-no-qualifying-type': [
-            true,
-            {
-                ignore: ['attribute'],
-            },
-        ],
-        'selector-pseudo-class-no-unknown': [
-            true,
-            {
-                ignorePseudoClasses: ['global', 'local'],
-            },
-        ],
-        'selector-type-no-unknown': [
-            true,
-            {
-                ignore: ['custom-elements'],
-                ignoreTypes: [/\^+/],
-            },
-        ],
-        'string-quotes': 'single',
-    },
+	extends: ['stylelint-config-standard'],
+	overrides: [
+		{
+			customSyntax: 'postcss-html',
+			files: [
+				'*.vue',
+				'**/*.vue',
+			],
+		},
+	],
+	plugins: ['stylelint-order'],
+	rules: {
+		'color-named': 'never',
+		'declaration-block-no-redundant-longhand-properties': [
+			true,
+			{ ignoreShorthands: ['/grid/'] },
+		],
+		'declaration-block-semicolon-newline-after': 'always',
+		'declaration-no-important': true,
+		'declaration-property-value-disallowed-list': { '/^border/': ['none'] },
+		'font-weight-notation': 'numeric',
+		'indentation': 'tab',
+		'max-line-length': null,
+		'max-nesting-depth': [
+			4,
+			{
+				ignore: ['pseudo-classes'],
+				ignoreAtRules: [
+					'media',
+					'supports',
+				],
+			},
+		],
+		'no-descending-specificity': null,
+		'number-max-precision': 6,
+		'order/order': [
+			[
+				'custom-properties',
+				'dollar-variables',
+				{
+					name: 'extend',
+					type: 'at-rule',
+				},
+				{
+					hasBlock: false,
+					name: 'include',
+					type: 'at-rule',
+				},
+				'declarations',
+				{
+					hasBlock: true,
+					name: 'include',
+					type: 'at-rule',
+				},
+				'rules',
+			],
+		],
+		'order/properties-alphabetical-order': true,
+		'selector-class-pattern': [
+			'^[a-z0-9\\-]+$',
+			{ message: 'Selector should be written in lowercase with hyphens' },
+		],
+		'selector-max-compound-selectors': 3,
+		'selector-max-id': 0,
+		'selector-no-qualifying-type': [
+			true,
+			{ ignore: ['attribute'] },
+		],
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: [
+					'global',
+					'local',
+				],
+			},
+		],
+		'selector-type-no-unknown': [
+			true,
+			{
+				ignore: ['custom-elements'],
+				ignoreTypes: [/\^+/],
+			},
+		],
+		'string-quotes': 'single',
+	},
 };
