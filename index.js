4633ef98948cdda121764123fa9e235275e246b8
Jochem <info@jochemkeller.nl>
2024-04-04 06:52:42 +0200

    feat: 1.4.0

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-/* eslint-disable unicorn/prefer-module */
-
-module.exports = {
-	extends: ['stylelint-config-standard', 'stylelint-config-html'],
-	plugins: ['stylelint-order', 'stylelint-stylistic'],
+/** @type {import('stylelint').Config} */
+export default {
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-html',
+	],
+	plugins: [
+		'stylelint-order',
+		'@stylistic/stylelint-plugin',
+	],
 	rules: {
-		'color-named': 'never',
-		'declaration-block-no-redundant-longhand-properties': [
-			true,
-			{ ignoreShorthands: ['/grid/'] },
-		],
+		/**
+		 * Stylelint
+		 * @see https://stylelint.io/user-guide/rules
+		 */
+
+		/* Avoid errors - Descending */
+		'no-descending-specificity': null,
+
+		/* Avoid errors - Invalid */
 		'declaration-no-important': true,
+
+		/* Avoid errors - Unknown */
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: [
+					'global',
+					'local',
+				],
+			},
+		],
+		'selector-type-no-unknown': [
+			true,
+			{
+				ignore: ['custom-elements'],
+				ignoreTypes: [/\^+/],
+			},
+		],
+
+		/* Enforce conventions - Allowed, disallowed & required - Color */
+		'color-named': 'never',
+
+		/* Enforce conventions - Allowed, disallowed & required - Declaration */
 		'declaration-property-value-disallowed-list': { '/^border/': ['none'] },
-		'font-weight-notation': 'numeric',
+
+		/* Enforce conventions - Max & min */
 		'max-nesting-depth': [
 			4,
 			{
@@ -22,8 +55,33 @@ module.exports = {
 				],
 			},
 		],
-		'no-descending-specificity': null,
 		'number-max-precision': 6,
+		'selector-max-compound-selectors': 3,
+		'selector-max-id': 0,
+		'selector-no-qualifying-type': [
+			true,
+			{ ignore: ['attribute'] },
+		],
+
+		/* Enforce conventions - Notation */
+		'font-weight-notation': 'numeric',
+
+		/* Enforce conventions - Pattern */
+		'selector-class-pattern': [
+			'^[a-z0-9\\-]+$',
+			{ message: 'Selector should be written in lowercase with hyphens' },
+		],
+
+		/* Enforce conventions - Redundant */
+		'declaration-block-no-redundant-longhand-properties': [
+			true,
+			{ ignoreShorthands: ['/grid/'] },
+		],
+
+		/**
+		 * stylelint-order
+		 * @see https://github.com/hudochenkov/stylelint-order?tab=readme-ov-file#rules
+		 */
 		'order/order': [
 			[
 				'custom-properties',
@@ -47,35 +105,20 @@ module.exports = {
 			],
 		],
 		'order/properties-alphabetical-order': true,
-		'selector-class-pattern': [
-			'^[a-z0-9\\-]+$',
-			{ message: 'Selector should be written in lowercase with hyphens' },
-		],
-		'selector-max-compound-selectors': 3,
-		'selector-max-id': 0,
-		'selector-no-qualifying-type': [
-			true,
-			{ ignore: ['attribute'] },
-		],
-		'selector-pseudo-class-no-unknown': [
-			true,
-			{
-				ignorePseudoClasses: [
-					'global',
-					'local',
-				],
-			},
-		],
-		'selector-type-no-unknown': [
-			true,
-			{
-				ignore: ['custom-elements'],
-				ignoreTypes: [/\^+/],
-			},
-		],
-		'stylistic/declaration-block-semicolon-newline-after': 'always',
-		'stylistic/indentation': 'tab',
-		'stylistic/max-line-length': null,
-		'stylistic/string-quotes': 'single',
+
+		/**
+		 * Stylelint Stylistic
+		 * @see https://github.com/stylelint-stylistic/stylelint-stylistic/blob/main/docs/user-guide/rules.md
+		 */
+
+		/* Declaration block */
+		'@stylistic/declaration-block-semicolon-newline-after': 'always',
+
+		/* String */
+		'@stylistic/string-quotes': 'single',
+
+		/* General / Sheet */
+		'@stylistic/indentation': 'tab',
+		'@stylistic/max-line-length': null,
 	},
 };
